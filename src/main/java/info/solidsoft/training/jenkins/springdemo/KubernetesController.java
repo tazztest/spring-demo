30c9c067da883555fc404cd3f8246b8da92c4c65
Marcin Zajączkowski <szpak@users.sourceforge.net>
2019-09-15 17:52:48 +0200

    Add Kubernetes controller

--- src/main/java/info/solidsoft/training/jenkins/springdemo/KubernetesController.java
+++ src/main/java/info/solidsoft/training/jenkins/springdemo/KubernetesController.java
@@ -1,4 +1,16 @@
 package info.solidsoft.training.jenkins.springdemo;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kubernetes")
 public class KubernetesController {
+
+    @GetMapping(value = "/status", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String status() {
+        return System.getenv("KUBERNETES_SERVICE_HOST") != null ? "Welcome from Kubernetes!" : "Kubernetes not found :(";
+    }
 }
